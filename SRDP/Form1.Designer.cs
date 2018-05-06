c3bdc17f81f566fef38a7b9a82fe56b75e5f80cf
Patrick Hees <Patrick.Hees@hotmail.com>
2018-05-06 23:22:11 +0200

    Initial commit

---
+++ SRDP/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace SRDP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitle2 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbComputerName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblComputerName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.DimGray;
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.lblTitle2);
+            this.splitContainer1.Panel1.Controls.Add(this.lblTitle);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnConnect);
+            this.splitContainer1.Panel2.Controls.Add(this.tbComputerName);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.btnCancel);
+            this.splitContainer1.Panel2.Controls.Add(this.lblComputerName);
+            this.splitContainer1.Size = new System.Drawing.Size(429, 229);
+            this.splitContainer1.SplitterDistance = 105;
+            this.splitContainer1.TabIndex = 99;
+            this.splitContainer1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTitle2
+            // 
+            this.lblTitle2.AutoSize = true;
+            this.lblTitle2.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle2.ForeColor = System.Drawing.Color.White;
+            this.lblTitle2.Location = new System.Drawing.Point(106, 45);
+            this.lblTitle2.Name = "lblTitle2";
+            this.lblTitle2.Size = new System.Drawing.Size(163, 37);
+            this.lblTitle2.TabIndex = 5;
+            this.lblTitle2.Text = "Connection";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(108, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(234, 30);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "Secure Remote Desktop";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(261, 85);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbComputerName
+            // 
+            this.tbComputerName.Location = new System.Drawing.Point(73, 42);
+            this.tbComputerName.Name = "tbComputerName";
+            this.tbComputerName.Size = new System.Drawing.Size(344, 20);
+            this.tbComputerName.TabIndex = 0;
+            this.tbComputerName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbComputerName_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter a full remote computer name and click Connect.";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(342, 85);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblComputerName
+            // 
+            this.lblComputerName.AutoSize = true;
+            this.lblComputerName.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblComputerName.Location = new System.Drawing.Point(12, 45);
+            this.lblComputerName.Name = "lblComputerName";
+            this.lblComputerName.Size = new System.Drawing.Size(55, 13);
+            this.lblComputerName.TabIndex = 0;
+            this.lblComputerName.Text = "Computer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 229);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Secure Remote Desktop Connection";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbComputerName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblComputerName;
+        private System.Windows.Forms.Label lblTitle2;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SRDP/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SRDP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Close Window and terminate application
+        private void btnCancel_Click(object sender, EventArgs e) => this.Close();
+
+        // Start a new RDP-Session with the entered computer name
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (tbComputerName.Text != "")
+            {
+
+                if (tbComputerName.Text.All(x => char.IsLetterOrDigit(x) || char.IsPunctuation(x))) {
+
+                    Process newRDPConnection = new Process();
+                    newRDPConnection.StartInfo.FileName = "mstsc.exe";
+                    newRDPConnection.StartInfo.Arguments = "/remoteguard /v:" + tbComputerName.Text;
+                    newRDPConnection.Start();
+                    this.Close();
+
+                }
+                else {
+
+                    MessageBox.Show(this, "The computer name is not valid.", "Secure Remote Desktop Connection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+            }
+            else {
+
+                MessageBox.Show(this, "Please enter a computer name.", "Secure Remote Desktop Connection",  MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        // Start a new RDP-Session with the entered computer name
+        private void tbComputerName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                if (tbComputerName.Text != "")
+                {
+
+                    if (tbComputerName.Text.All(x => char.IsLetterOrDigit(x) || char.IsPunctuation(x)))
+                    {
+
+                        Process newRDPConnection = new Process();
+                        newRDPConnection.StartInfo.FileName = "mstsc.exe";
+                        newRDPConnection.StartInfo.Arguments = "/remoteguard /v:" + tbComputerName.Text;
+                        newRDPConnection.Start();
+                        this.Close();
+
+                    }
+                    else
+                    {
+
+                        MessageBox.Show(this, "The computer name is not valid.", "Secure Remote Desktop Connection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+
+                }
+                else
+                {
+
+                    MessageBox.Show(this, "Please enter a computer name.", "Secure Remote Desktop Connection", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+        }
+    }
+}
